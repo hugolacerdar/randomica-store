b2126fe7929e321915a106273ec9233a76ee2925
Hugo <hugolacerdar@gmail.com>
2021-08-30 19:07:08 -0300

    Add Page component

---
+++ frontend/components/Page.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import Header from './Header';
+
+export default function Page({ children }) {
+  return (
+    <div>
+      <Header />
+      <h2>I am the page component</h2>
+      {children}
+    </div>
+  );
+}
+
+Page.propTypes = {
+  children: PropTypes.oneOf([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};

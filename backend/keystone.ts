ffcfe4b8bf5a7011a4f24f3e9c38c4cd84e36b12
Hugo <hugolacerdar@gmail.com>
2021-09-01 10:00:35 -0300

    Add Product schema

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -6,6 +6,7 @@ import {
   statelessSessions,
 } from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
+import { Product } from './schemas/Product';
 
 const databaseURL =
   process.env.DATABESE_URL || 'mongodb://localhost/keystone-randomica-store';
@@ -41,6 +42,7 @@ export default withAuth(
     lists: createSchema({
       // schema items
       User,
+      Product,
     }),
     ui: {
       isAccessAllowed: ({ session }): boolean => !!session?.data,

20f0b456735a44272d9947a1ebf02c1e1c7f17eb
Hugo <hugolacerdar@gmail.com>
2021-09-02 16:41:05 -0300

    Update default font-size

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -17,6 +17,7 @@ const GlobalStyles = createGlobalStyle`
     --maxWidth: 1000px;
     --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
     box-sizing: border-box;
+    font-size: 62.5%;
   }
 
   *, *:before, *:after {
@@ -46,7 +47,7 @@ const GlobalStyles = createGlobalStyle`
 `;
 
 const InnerStyles = styled.div`
-  max-width: var(--max-width);
+  max-width: var(--maxWidth);
   margin: 0 auto;
   padding: 2rem;
 `;

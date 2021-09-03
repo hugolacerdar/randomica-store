c1633e5feb7962a2bb89180a574705ef4e2a8da8
Hugo <hugolacerdar@gmail.com>
2021-09-03 10:24:11 -0300

    Add custom route on submit

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,9 +1,11 @@
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
 import { useRef } from 'react';
+import Router from 'next/router';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
 import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
 
 const CREATE_PRODUCT_MUTATION = gql`
   mutation CREATE_PRODUCT_MUTATION(
@@ -42,6 +44,7 @@ export default function CreateProduct() {
     CREATE_PRODUCT_MUTATION,
     {
       variables: inputs,
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
     }
   );
 
@@ -49,8 +52,11 @@ export default function CreateProduct() {
     <Form
       onSubmit={async (e) => {
         e.preventDefault();
-        await createProduct();
+        const res = await createProduct();
         clearForm(fileInputRef);
+        Router.push({
+          pathname: `/product/${res.data.createProduct.id}`,
+        });
       }}
     >
       <DisplayError error={error} />

fa3e2efc7129bac82c9a3307cc90b9054836440a
Hugo <hugolacerdar@gmail.com>
2021-09-02 21:37:02 -0300

    Add color vars

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -12,7 +12,9 @@ const GlobalStyles = createGlobalStyle`
   html {
     --black: #393939;
     --grey: #3A3A3A;
+    --gray: #3A3A3A;
     --lightGrey: #E1E1E1;
+    --lightGray: #E1E1E1;
     --offWhite: #EDEDED;
     --maxWidth: 1000px;
     --bs: 0 12px 24px 0 rgba(0,0,0,0.09);

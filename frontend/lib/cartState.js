6bace17d19f128b450692d3e13866337e6e4d22e
Hugo <hugolacerdar@gmail.com>
2021-09-08 20:32:06 -0300

    Add cart context

---
+++ frontend/lib/cartState.js
@@ -0,0 +1,40 @@
+import { createContext, useContext, useState } from 'react';
+import PropTypes from 'prop-types';
+
+const LocalStateContext = createContext();
+const LocalStateProvider = LocalStateContext.Provider;
+
+function CartStateProvider({ children }) {
+  const [cartOpen, setCartOpen] = useState(false);
+
+  function toggleCart() {
+    setCartOpen(!cartOpen);
+  }
+
+  function closeCart() {
+    setCartOpen(false);
+  }
+
+  function openCart() {
+    setCartOpen(true);
+  }
+
+  return (
+    <LocalStateProvider
+      value={{ cartOpen, setCartOpen, toggleCart, closeCart, openCart }}
+    >
+      {children}
+    </LocalStateProvider>
+  );
+}
+
+CartStateProvider.propTypes = {
+  children: PropTypes.any,
+};
+
+function useCart() {
+  const all = useContext(LocalStateContext);
+  return all;
+}
+
+export { CartStateProvider, useCart };

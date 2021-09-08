b267f39cabc6745e6ff1c2640d132a69017f7f4c
Hugo <hugolacerdar@gmail.com>
2021-09-08 14:33:17 -0300

    Add cart item relationship

--- backend/schemas/User.ts
+++ backend/schemas/User.ts
@@ -1,6 +1,5 @@
 import { list } from '@keystone-next/keystone/schema';
 import { text, password, relationship } from '@keystone-next/fields';
-import { CartItem } from './CartItem';
 
 export const User = list({
   // access:

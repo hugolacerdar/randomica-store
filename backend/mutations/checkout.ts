0fd015effcf93840f83d131d359d132197ee129e
Hugo <hugolacerdar@gmail.com>
2021-09-09 17:58:36 -0300

    Add Checkout mutation

---
+++ backend/mutations/checkout.ts
@@ -0,0 +1,95 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { KeystoneContext } from '@keystone-next/types';
+import {
+  CartItemCreateInput,
+  OrderCreateInput,
+} from '../.keystone/schema-types';
+import stripeConfig from '../lib/stripe';
+
+const graphql = String.raw;
+
+export default async function checkout(
+  root: any,
+  { token }: { token: string },
+  context: KeystoneContext
+): Promise<OrderCreateInput> {
+  const userId = context.session.itemId;
+  if (!userId) {
+    throw new Error('Sorry, users must be signed in to create an order');
+  }
+  const user = await context.lists.User.findOne({
+    where: { id: userId },
+    resolveFields: graphql`
+    id
+    name
+    email
+    cart {
+      id
+      quantity
+      product {
+        name
+        price
+        description
+        id
+        photo {
+          id
+          image {
+            id
+            publicUrlTransformed
+          }
+        }
+      }
+    }`,
+  });
+
+  const cartItems = user.cart.filter((cartItem) => cartItem.product);
+  const amount = cartItems.reduce(function (
+    tally: number,
+    cartItem: CartItemCreateInput
+  ) {
+    return tally + cartItem.quantity * cartItem.product.price;
+  },
+  0);
+
+  const charge = await stripeConfig.paymentIntents
+    .create({
+      amount,
+      currency: 'USD',
+      confirm: true,
+      payment_method: token,
+    })
+    .catch((err) => {
+      console.log(err);
+      throw new Error(err.message);
+    });
+
+  const orderItems = cartItems.map((cartItem) => {
+    const orderItem = {
+      name: cartItem.product.name,
+      description: cartItem.product.description,
+      price: cartItem.product.price,
+      quantity: cartItem.quantity,
+      photo: { connect: { id: cartItem.product.photo.id } },
+    };
+
+    return orderItem;
+  });
+
+  const order = await context.lists.Order.createOne({
+    data: {
+      total: charge.amount,
+      charge: charge.id,
+      items: { create: orderItems },
+      user: { connect: { id: userId } },
+    },
+  });
+
+  const cartItemsIds = cartItems.map((cartItem) => cartItem.id);
+  await context.lists.CartItem.deleteMany({
+    ids: cartItemsIds,
+  });
+
+  return order;
+}

1d67289d47f1877aba4a937bf09a01993ed38389
Hugo <hugolacerdar@gmail.com>
2021-09-09 18:01:53 -0300

    Add Checkout component

---
+++ frontend/components/Checkout.js
@@ -0,0 +1,94 @@
+/* eslint-disable no-unused-expressions */
+import { useMutation } from '@apollo/client';
+import {
+  CardElement,
+  Elements,
+  useElements,
+  useStripe,
+} from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import gql from 'graphql-tag';
+import { useRouter } from 'next/dist/client/router';
+import nProgress from 'nprogress';
+import { useState } from 'react';
+import styled from 'styled-components';
+import SickButton from './styles/SickButton';
+
+const CheckoutFormStyles = styled.form`
+  box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
+  border: 1px solid rgba(0, 0, 0, 0.06);
+  border-radius: 0.5rem;
+  padding: 1rem;
+  display: grid;
+  grid-gap: 1rem;
+`;
+
+const CREATE_ORDER_MUTATION = gql`
+  mutation CREATE_ORDER_MUTATION($token: String!) {
+    checkout(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        name
+      }
+    }
+  }
+`;
+
+const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
+function CheckoutForm() {
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [checkout, { error: gqlError }] = useMutation(CREATE_ORDER_MUTATION);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+    nProgress.start();
+
+    const { paymentMethod, error } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+    });
+
+    if (error) {
+      setError(error);
+      nProgress.done();
+      return;
+    }
+
+    if (!error) setError();
+
+    const order = await checkout({
+      variables: {
+        token: paymentMethod.id,
+      },
+    });
+
+    console.log('finished order: ', order);
+
+    setLoading(false);
+    nProgress.done();
+  }
+  return (
+    <CheckoutFormStyles onSubmit={handleSubmit}>
+      {error && <p style={{ fontSize: 12 }}>🔴 {error.message}</p>}
+      {gqlError && <p style={{ fontSize: 12 }}>🔴 {gqlError.message}</p>}
+      <CardElement />
+      <SickButton>Checkout Now</SickButton>
+    </CheckoutFormStyles>
+  );
+}
+
+export default function Checkout() {
+  return (
+    <Elements stripe={stripeLib}>
+      <CheckoutForm />
+    </Elements>
+  );
+}
